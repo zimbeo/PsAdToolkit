64d85ead437aab747d6d373ef2754ba9e8f5aa9c
Henry Rohlfing <henryrohlfing2@gmail.com>
2022-05-03 16:43:43 -0500

    broke the test oops

--- Tests/Unit.Tests.ps1
+++ Tests/Unit.Tests.ps1
@@ -8,9 +8,10 @@ $ManifestFile = "$(Split-Path (Split-Path -Parent -Path $MyInvocation.MyCommand.
 
 # Import the module and store the information about the module
 $ModuleInformation = Import-Module -Name $ManifestFile -PassThru
-
-It 'should validate the module manifest' {
-	{ Test-ModuleManifest -Path $ManifestFile -ErrorAction Stop } | Should -Not throw
+Describe 'Manifest Validation tests' {
+	It 'should validate the module manifest' {
+		{ Test-ModuleManifest -Path $ManifestFile -ErrorAction Stop } | Should -Not throw
+	}
 }
 
 Describe 'PSScriptAnalyzer tests' {
