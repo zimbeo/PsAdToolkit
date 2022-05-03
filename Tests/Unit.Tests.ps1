1aadadaee737b46a36a9055457826fd25bee7d38
Henry Rohlfing <henryrohlfing2@gmail.com>
2022-05-03 16:41:37 -0500

    fixed? I think

--- Tests/Unit.Tests.ps1
+++ Tests/Unit.Tests.ps1
@@ -9,36 +9,9 @@ $ManifestFile = "$(Split-Path (Split-Path -Parent -Path $MyInvocation.MyCommand.
 # Import the module and store the information about the module
 $ModuleInformation = Import-Module -Name $ManifestFile -PassThru
 
-#Describe "$ModuleName Module - Testing Manifest File (.psd1)" {
-	# Context "Manifest" {
-	# 	It "Should contains RootModule" {
-	# 		$ModuleInformation.RootModule | Should -NotBeNullOrEmpty
-	# 	}
-
-	# 	It "Should contains Author" {
-	# 		$ModuleInformation.Author | Should -NotBeNullOrEmpty
-	# 	}
-
-	# 	It "Should contains Company Name" {
-	# 		$ModuleInformation.CompanyName | Should -NotBeNullOrEmpty
-	# 	}
-
-	# 	It "Should contains Description" {
-	# 		$ModuleInformation.Description | Should -NotBeNullOrEmpty
-	# 	}
-
-	# 	It "Should contains Copyright" {
-	# 		$ModuleInformation.Copyright | Should -NotBeNullOrEmpty
-	# 	}
-
-	# 	It "Should contains a Project Link" {
-	# 		$ModuleInformation.ProjectURI | Should -NotBeNullOrEmpty
-	# 	}
-
-	# 	It "Should contains a Tags (For the PSGallery)" {
-	# 		$ModuleInformation.Tags.count | Should -NotBeNullOrEmpty
-	# 	}
-	# }
+It 'should validate the module manifest' {
+	{ Test-ModuleManifest -Path $ManifestFile -ErrorAction Stop } | Should -Not throw
+}
 
 Describe 'PSScriptAnalyzer tests' {
 
